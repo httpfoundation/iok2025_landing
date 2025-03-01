e4f9f9fa6019189b31138be0b1e0c7e2d3c88791
Oliver Mrakovics <oliver.mrakovics@gmail.com>
2025-03-01 14:51:08 +0100

    statistics from Dato

--- src/sections/Overlay/Overlay.jsx
+++ src/sections/Overlay/Overlay.jsx
@@ -2,9 +2,13 @@ import Book from "../../icons/Book";
 import Coffee from "../../icons/Coffee";
 import Mic from "../../icons/Mic";
 import Person from "../../icons/Person";
+import { useStaticElement } from "../../tools/datoCmsTools";
 import "./Overlay.scss";
 
 const Overlay = () => {
+  const [statistics] = useStaticElement("statistics", false);
+  if (!statistics) return null;
+
   return (
     <div id="overlay">
       <div className="bg bg-1"></div>
@@ -14,28 +18,28 @@ const Overlay = () => {
           <div className="infobox">
             <Mic />
             <div>
-              <div className="title">?</div>
+              <div className="title">{statistics.talks}</div>
               <div className="subtitle">előadás</div>
             </div>
           </div>
           <div className="infobox">
             <Person />
             <div>
-              <div className="title">?</div>
+              <div className="title">{statistics.speakers}</div>
               <div className="subtitle">előadó</div>
             </div>
           </div>
           <div className="infobox">
             <Coffee />
             <div>
-              <div className="title">250</div>
+              <div className="title">{statistics.onsiteSize}</div>
               <div className="subtitle">helyszíni férőhely</div>
             </div>
           </div>
           <div className="infobox">
             <Book />
             <div>
-              <div className="title">1000+</div>
+              <div className="title">{statistics.onlineSize}</div>
               <div className="subtitle">online férőhely</div>
             </div>
           </div>
